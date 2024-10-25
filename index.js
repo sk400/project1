e199680ae6b19ae8fc5f227ae10d3b7fb64addc8
[sk developer] <[skdeveloper101@gmail.com]>
2024-10-25 11:16:38 +0530

    Add two new users

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const data = [{
-  name: "user1",
-  age: 15,
-}]
+const data = [
+  {
+    name: "user1",
+    age: 15,
+  },
+  {
+    name: "user2",
+    age: 18,
+  },
+  {
+    name: "user3",
+    age: 20,
+  },
+];
